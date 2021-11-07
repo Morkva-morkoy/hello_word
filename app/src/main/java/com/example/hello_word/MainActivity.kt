5299ae2b09edd356c2ae5f3f0165b3206fcbc5ed
SHLYAPA <stepa2429999@gmail.com>
2021-11-07 22:14:59 +0300

    App center created

--- app/src/main/java/com/example/hello_word/MainActivity.kt
+++ app/src/main/java/com/example/hello_word/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.hello_word
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
-
     private var textID: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
