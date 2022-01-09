b637490ce8729a0892da8291c55c6e6e902719c1
Roman Rusinov <roman.a.rusinov@gmail.com>
2022-01-09 20:04:24 +0300

    getting route info

--- app/src/main/java/com/example/hello_word/MainActivity.kt
+++ app/src/main/java/com/example/hello_word/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import java.net.URL
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.widget.EditText
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textus: TextView
     private lateinit var but: Button
     private lateinit var mapsus: TextView
+    private lateinit var originus: EditText
+    private lateinit var destinationus: EditText
+    private lateinit var but2: Button
 
 
     fun getRoute(url: String): Array<String> {
@@ -34,20 +38,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var route_info = getRoute("https://maps.googleapis.com/maps/api/directions/json?origin=Budapest&destination=Bogatyrsky%20Ave&region=es&key=AIzaSyBZ3qf0Ir4UFjBREN3bzkPG4fm0KWbk83o")
-        var n = 2
-        for (i in route_info) {
-            if (i.contains("duration")) {
-                break
-            }
-            n = n + 1
-        }
+//        var route_info = getRoute("https://maps.googleapis.com/maps/api/directions/json?origin=Budapest&destination=Bogatyrsky%20Ave&region=es&key=AIzaSyBZ3qf0Ir4UFjBREN3bzkPG4fm0KWbk83o")
+//        var n = 2
+//        for (i in route_info) {
+//            if (i.contains("duration")) {
+//                break
+//            }
+//            n = n + 1
+//        }
 
         but = findViewById(R.id.button)
         textus = findViewById(R.id.textView)
         mapsus = findViewById(R.id.textView3)
         textus.setTextColor(Color.parseColor("#673AB7"))
-        mapsus.text = route_info[n]
+        originus = findViewById(R.id.Origin)
+        destinationus = findViewById(R.id.Destination)
+        but2 = findViewById(R.id.button2)
 //  }
 
 
@@ -58,6 +64,21 @@ class MainActivity : AppCompatActivity() {
             else{
                 textus.setTextColor(Color.parseColor("#673AB7"))
             }
+        }
+
+        but2.setOnClickListener {
+            var origin = originus.text
+            var destination = destinationus.text
+            var urlus: String = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin.toString().replace(" ", "%20") + "&destination=" + destination.toString().replace(" ", "%20") + "&key=AIzaSyBZ3qf0Ir4UFjBREN3bzkPG4fm0KWbk83o"
+            var route_info = getRoute(urlus)
+            var n = 1
+            for (i in route_info) {
+                if (i.contains("duration")) {
+                    break
+                }
+                n = n + 1
+            }
+            mapsus.text = route_info[n]
         }
     }
 }
